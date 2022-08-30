ae7be1ed6ff61e2d1aad87ea57380aa88073272f
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-08-30 19:36:24 +0900

    음악 스트리밍 앱 - 03 (음악 목록 API 만들기)

--- MusicStreamingApp/PlayerFragment.kt
+++ MusicStreamingApp/PlayerFragment.kt
@@ -1,13 +1,41 @@
 package com.example.musicstreamingapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.musicstreamingapp.service.MusicDto
+import com.example.musicstreamingapp.service.MusicService
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class PlayerFragment : Fragment(R.layout.fragment_player) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        getVideoListFromServer()
+    }
+
+    private fun getVideoListFromServer() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://run.mocky.io")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(MusicService::class.java)
+            .also {
+                it.listMusics()
+                    .enqueue(object : Callback<MusicDto> {
+                        override fun onResponse(call: Call<MusicDto>, response: Response<MusicDto>) {
+                            Log.d("PlayerFragment", "${response.body()}")
+                        }
+
+                        override fun onFailure(call: Call<MusicDto>, t: Throwable) {
+
+                        }
+
+                    })
+            }
     }
 
     companion object {
---
+++ MusicStreamingApp/service/MusicService.kt
@@ -0,0 +1,9 @@
+package com.example.musicstreamingapp.service
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MusicService {
+    @GET("/v3/b3e235f9-bc96-4ffd-9bc3-2cc49944454c")
+    fun listMusics() : Call<MusicDto>
+}
