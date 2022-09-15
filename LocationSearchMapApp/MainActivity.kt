6e039d6af091ba54cefd6e7571311fa288e91186
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-09-15 20:22:30 +0900

    위치검색 지도 앱 - 07 (Tmap POI 데이터 소개 및 데이터 뿌리기 - 1 [끝])

--- LocationSearchMapApp/MainActivity.kt
+++ LocationSearchMapApp/MainActivity.kt
@@ -2,14 +2,22 @@ package com.example.locationsearchmapapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.view.isVisible
 import com.example.locationsearchmapapp.databinding.ActivityMainBinding
-import kotlinx.coroutines.Job
+import com.example.locationsearchmapapp.utility.RetrofitUtil
+import kotlinx.coroutines.*
+import retrofit2.Retrofit
+import java.lang.Exception
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
 
     private lateinit var job : Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: SearchRecyclerAdapter
@@ -19,8 +27,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        job = Job()
+
         initAdapter()
         initView()
+        bindViews()
         initData()
         setData()
     }
@@ -28,6 +39,12 @@ class MainActivity : AppCompatActivity() {
     private fun initView() = with(binding) {
         emptyResultTextView.isVisible = false
         recyclerView.adapter = adapter
+    }
+
+    private fun bindViews() = with(binding) {
+        searchButton.setOnClickListener {
+            searchKeyword(searchBarInputView.text.toString())
+        }
     }
 
     private fun initAdapter() {
@@ -52,6 +69,27 @@ class MainActivity : AppCompatActivity() {
         adapter.setSearchResultList(dataList) {
             Toast.makeText(this, "할당이름 : ${it.name}, 주소 : ${it.fullAddress}", Toast.LENGTH_SHORT)
                 .show()
+        }
+    }
+
+    private fun searchKeyword(keywordString : String) {
+        launch(coroutineContext) {
+            try {
+                withContext(Dispatchers.IO) {
+                    val response = RetrofitUtil.apiService.getSearchLocation(
+                        keyword = keywordString
+                    )
+                    if (response.isSuccessful) {
+                        val body = response.body()
+                        withContext(Dispatchers.Main) {
+                            Log.e("respone", body.toString())
+                        }
+                    }
+                }
+            } catch (e : Exception) {
+                e.printStackTrace()
+                Toast.makeText(this@MainActivity, "검색하는 과정에서 에러가 발생했습니다. : ${e.message}", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
