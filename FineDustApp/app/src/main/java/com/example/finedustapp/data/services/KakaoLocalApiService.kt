e02d40e432b4b0729a4da2319a1d2844feeefd2a
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2023-01-10 22:55:29 +0900

    카카오 API 에러 해결
    Header -> Headers

--- FineDustApp/app/src/main/java/com/example/finedustapp/data/services/KakaoLocalApiService.kt
+++ FineDustApp/app/src/main/java/com/example/finedustapp/data/services/KakaoLocalApiService.kt
@@ -4,11 +4,11 @@ import com.example.finedustapp.BuildConfig
 import com.example.finedustapp.data.models.tmcoordinates.TmCoordinatesResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface KakaoLocalApiService {
-//    @Header("Authorization: KakaoAK ${BuildConfig.KAKAO_API_KEY}")
+    @Headers("Authorization: KakaoAK ${BuildConfig.KAKAO_API_KEY}")
     @GET("/v2/local/geo/transcoord.json?output_coord=TM")
     suspend fun getTmCoordinates(
         @Query("REST_API_KEY") REST_API_KEY: String,
